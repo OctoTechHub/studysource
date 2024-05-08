5b7ee15f355f74dc5f5ba7416d864934ab0dec4e
Krish Soni <sonikrish994@gmail.com>
2024-05-08 20:56:16 +0530

    root structure

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+
+export default function Navbar(){
+    return (
+        <nav>
+            <ul>
+                <h1>Study-Material</h1>
+                <li>About</li>
+                <li>Maintainers</li>
+            </ul>
+        </nav>
+    )
+}
