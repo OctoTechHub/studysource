6ee9af7224a439b4d9ad4a56ebf4e6f378e8e7dd
KRISH SONI <67964054+krishvsoni@users.noreply.github.com>
2024-06-08 22:08:39 +0530

    fixed

--- src/material.ts
+++ src/material.ts
@@ -2,10 +2,10 @@ export const materials = [
   {
     title: 'Odd Sem Time Table 2024-2025',
     driveLink: 'https://drive.google.com/drive/folders/1Zz9jmbsaYiATta02ijN1tIFucZiysF_E?usp=drive_link',
-    semester: 5-7,
+    semester: 3-5-7,
     department: 'All Department',
 
-  }
+  },
   
   {
     title: 'Sem 1 All Subjects',
